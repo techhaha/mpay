bab5fa4f539666f0614275d2ca923bfe2433b06e
技术老胡 <1094551889@qq.com>
2024-11-02 17:59:36 +0800

    后台添加支付测试功能

--- app/controller/OrderController.php
+++ app/controller/OrderController.php
@@ -6,6 +6,7 @@ namespace app\controller;
 
 use app\BaseController;
 use app\model\Order;
+use app\model\User;
 use think\facade\View;
 
 class OrderController extends BaseController
@@ -26,5 +27,19 @@ class OrderController extends BaseController
         } else {
             return '订单不存在';
         }
+    }
+    public function testPay()
+    {
+        $pid = 1000;
+        if (session('?pid')) {
+            $pid = session('pid');
+        }
+        View::assign('pid', $pid);
+        $key = User::where('pid', $pid)->where('state', 1)->value('secret_key');
+        if (!$key) {
+            return '用户禁用或不存在';
+        }
+        View::assign('key', $key);
+        return View::fetch();
     }
 }

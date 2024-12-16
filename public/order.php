e9f64988a9f5fc24f912a9800ff90a37c3f94f12
技术老胡 <1094551889@qq.com>
2024-12-16 14:15:27 +0800

    更新文件

--- public/order.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('content-type: application/json; charset=utf-8');
-$path = '../runtime/order.json';
-if (!file_exists($path)) {
-    exit('{"code":3,"msg":"文件不存在"}');
-} else {
-    exit(file_get_contents($path));
-}

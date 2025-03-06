62181cf33d7cc6f29df48ef8fca17e214768a3cb
技术老胡 <1094551889@qq.com>
2025-03-06 21:05:10 +0800

    更新文档

--- config/extend/payplugin.php
+++ config/extend/payplugin.php
@@ -18,18 +18,6 @@ return array (
   ),
   1 => 
   array (
-    'platform' => 'sqbpay',
-    'name' => '收钱吧',
-    'class_name' => 'ShouQianBa',
-    'price' => NULL,
-    'describe' => '主流移动支付全能收 信用卡,花呗都能用,生意帮手收钱吧,移动收款就用它!',
-    'website' => 'https://www.shouqianba.com',
-    'helplink' => '',
-    'version' => '1.0',
-    'state' => 1,
-  ),
-  2 => 
-  array (
     'platform' => 'alipay',
     'name' => '支付宝',
     'class_name' => 'AliPay',
@@ -37,6 +25,18 @@ return array (
     'describe' => '支持支付宝个人收款码、经营码收款，监听回调',
     'website' => 'https://www.alipay.com',
     'helplink' => '',
+    'version' => '1.0',
+    'state' => 1,
+  ),
+  2 => 
+  array (
+    'platform' => 'sqbpay',
+    'name' => '收钱吧',
+    'class_name' => 'ShouQianBa',
+    'price' => NULL,
+    'describe' => '主流移动支付全能收 信用卡,花呗都能用,生意帮手收钱吧,移动收款就用它!',
+    'website' => 'https://www.shouqianba.com',
+    'helplink' => 'https://f0bmwzqjtq2.feishu.cn/docx/WwN7dRqmuoozlIxgiuFclZhCnte',
     'version' => '1.0',
     'state' => 1,
   ),
